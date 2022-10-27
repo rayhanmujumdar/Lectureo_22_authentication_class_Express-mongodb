2915be3f95adec9ed72efd2337773ab6a2cd2c63
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-10-27 10:44:57 -0500

    user create api completed

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const connectDB = require("./db");
-
+const User = require("./models/User");
+const bcrypt = require('bcrypt')
+// middleware
 const app = express();
-app.post('/register',(req,res) => {
-    /**
-     * Request input sources:
-     * - req body
-     * - req params
-     * - req header
-     * - req cookies
-     */
-})
+app.use(express.json());
 
-
+app.post("/register", async (req, res) => {
+  /**
+   * Request input sources:
+   * - req body
+   * - req params
+   * - req header
+   * - req cookies
+   */
+  const { name, email, password } = req?.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "invalid" });
+  }
+  // exec() use to if you find any error in your function
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "user already exist" });
+  }
+  user = new User({
+    name,
+    email,
+    password,
+  });
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password,salt)
+  user.password = hash
+  await user.save();
+  return res.status(201).json({ message: "user created successfully", user });
+});
 
 app.get("/", (_req, res) => {
   const obj = {
@@ -21,7 +42,6 @@ app.get("/", (_req, res) => {
   };
   res.json(obj);
 });
-
 
 connectDB("mongodb://localhost:27017/attendance-db")
   .then(() => {

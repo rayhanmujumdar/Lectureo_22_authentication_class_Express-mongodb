4cdc6969c3d67d4da89ae4782d292671c293f95e
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-11-02 11:58:05 -0500

    authentication comment added

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,22 @@ const { registerController, logInController } = require("../controllers/auth");
 
 const router = require("express").Router();
 
+/**
+ * create a new authentication user
+ * @api api/v1/register
+ * @apiDescription create a new user by name,email,password,roles,accountStatus or etc input
+ * @visibility Public
+ * @method POST
+ */
 router.post("/register", registerController);
+
+/**
+ * authentication login
+ * @api api/v1/login
+ * @apiDescription email or password to authenticate user
+ * @visibility Public
+ * @method POST
+ */
 
 router.post("/login", logInController);
 

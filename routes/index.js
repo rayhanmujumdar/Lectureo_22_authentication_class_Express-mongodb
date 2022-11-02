de6ece41f8b63e57e164afd588b882ca29ae1441
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-11-02 11:54:17 -0500

    user CURD operation file setup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router()
 const authRoutes = require('./auth')
+const userRouter = require('./users')
 
 routes.use('/api/v1/auth/',authRoutes)
+routes.use('/api/v1/users',userRouter)
 
 module.exports = routes
---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const router = require('express').Router()
+
+/**
+ * user find by id
+ * @api api/v1/users/:userID
+ * @apiDescription user find by id to db
+ * @visibility Private
+ * @method GET
+ */
+
+router.get('/:userId',() => {})
+
+/**
+ * Update user
+ * @api api/v1/users/:userID
+ * @apiDescription find user to db and then update and then another set to DB
+ * @visibility Private
+ * @method PATCH
+ */
+
+router.patch('/:userId',() => {})
+
+/**
+ * Delete user
+ * @api api/v1/users/:userID
+ * @apiDescription user find by id and delete to DB
+ * @visibility Private
+ * @method DELETE
+ */
+
+router.delete('/:userId',() => {})
+
+/**
+ * find all users
+ * @api api/v1/users
+ * @apiDescription find all users to db
+ * @visibility Private
+ * @method GET
+ */
+
+router.get('/',() => {})
+
+/**
+ * create a new users
+ * @api api/v1/users
+ * @apiDescription create a new user and then set to DB
+ * @visibility private
+ * @method POST
+ */
+
+router.post('/',() => {})
+
+module.exports = router
